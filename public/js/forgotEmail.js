59891203862b8ffdc3db77b71c3db412a6a4b450
Aman Shaikh <amanshaikh8624@gmail.com>
2023-12-07 23:43:15 -0800

    some changes

--- public/js/forgotEmail.js
+++ public/js/forgotEmail.js
@@ -9,7 +9,7 @@ async function sendEmail (event) {
     console.log(email);
 
     // Send a POST request to the server
-    const response = await axios.post('http://54.152.126.236/forgotPassword/', {
+    const response = await axios.post('http://54.152.126.236:3000/forgotPassword/', {
         email: email, 
     });
 
