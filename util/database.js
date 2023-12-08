cb9cef33a2d8ff9479bbfec6ddd5a53c2f235f67
Aman Shaikh <amanshaikh8624@gmail.com>
2023-12-08 00:42:42 -0800

    new changes

--- util/database.js
+++ util/database.js
@@ -2,13 +2,20 @@ const Sequelize = require("sequelize");
 
 
 
-const sequelize = new Sequelize('ExpenseTracker', 'admin', 'Aman8624$', {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
   
   dialect: "mysql",
-  host: 'database-2.cxmtgoj1no82.us-east-1.rds.amazonaws.com',
+  host: process.env.DB_HOST,
 },);
 
 module.exports = sequelize;
+// const sequelize = new Sequelize('ExpenseTracker', 'admin', 'Aman8624$', {
+  
+//   dialect: "mysql",
+//   host: 'database-2.cxmtgoj1no82.us-east-1.rds.amazonaws.com',
+// },);
+
+// module.exports = sequelize;
 
 
 // const Sequelize = require("sequelize");
