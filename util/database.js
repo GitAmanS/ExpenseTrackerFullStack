7bf2b6686b7ff72f1025ad3a0a34414d3943b3e1
Aman Shaikh <amanshaikh8624@gmail.com>
2023-12-09 07:24:29 -0800

    env fixes

--- util/database.js
+++ util/database.js
@@ -18,7 +18,8 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize('ExpenseTracker', 'admin', 'Aman8624$', {
   
   dialect: "mysql",
-  host: 'database-2.cxmtgoj1no82.us-east-1.rds.amazonaws.com',
+  host: 'database-1.cxmtgoj1no82.us-east-1.rds.amazonaws.com',
+  
 },);
 
 module.exports = sequelize;
